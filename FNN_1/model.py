681ee8f65307484276ea2a1a673b6feec0c476f5
M Raffy Zeidan <mohammad.raffy@ui.ac.id>
2025-04-11 02:38:59 +0700

    Benerin

--- FNN_1/model.py
+++ FNN_1/model.py
@@ -37,7 +37,7 @@ class FNN(object):
         self.class_labels = {0: 'negative', 1: 'positive'}
         self.stop_words = set()
 
-    def initialize_model(self, ae_weights=None, gamma=0.1, eta=1.0, optimizer=SGD(lr=0.001, momentum=0.9)):
+    def initialize_model(self, ae_weights=None, gamma=0.1, eta=1.0, optimizer=SGD(learning_rate=0.001, momentum=0.9)):
         if ae_weights is not None:
             self.autoencoder.load_weights(ae_weights)
             print('Pretrained AE weights are loaded successfully.')
